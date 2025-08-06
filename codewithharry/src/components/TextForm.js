9619330edbdb7e49137ea42f0874887428dfe44b
prabin738 <prabinsingh750@gmail.com>
2025-08-06 09:38:25 +0545

    lots of new feature added

--- codewithharry/src/components/TextForm.js
+++ codewithharry/src/components/TextForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter your text here");
+  const [text, setText] = useState(" ");
   const handleUpClick = () => {
     // console.log("Uppercase was clicked" + text);
     let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.toLowerCase();
     setText(newText);
   };
   const handleOnChange = (event) => {
@@ -13,21 +18,35 @@ export default function TextForm(props) {
   };
 
   return (
-    <div>
-      <h1> {props.heading} </h1>
+    <>
+      <div className="container">
+        <h1> {props.heading} </h1>
 
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="18"
-        ></textarea>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="10"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-9">
+        <h2>Your Text summary</h2>
+        <p>
+          {text.split(" ").length} Words and {text.length} Characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Average minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
