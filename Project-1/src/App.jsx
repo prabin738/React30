a4d9bff6e65d0b503bc7bef510450de960fffc96
prabin738 <prabinsingh750@gmail.com>
2025-08-10 18:36:59 +0545

    navbar added

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div className="app">Hello World</div>
-    </>
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Home</li>
+          <li href="#">About</li>
+          <li href="#">Location</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
   );
-}
+};
 
 export default App;
