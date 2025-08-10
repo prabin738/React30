e89ce8051ae799d91a2a8495b35e4023d62de922
prabin738 <prabinsingh750@gmail.com>
2025-08-10 21:57:05 +0545

    navigation addded

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,18 +1,9 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li href="#">Home</li>
-          <li href="#">About</li>
-          <li href="#">Location</li>
-          <li href="#">Contact</li>
-        </ul>
-        <button>Login</button>
-      </nav>
+      <Navigation />
     </div>
   );
 };
