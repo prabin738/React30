697fc4adacdc844783a1b497381203a5f8dfe65a
prabin738 <prabinsingh750@gmail.com>
2025-08-11 17:57:55 +0545

    Initial Setup done

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import HeroSection from "./components/Hero";
 import Navigation from "./components/Navigation";
 const App = () => {
   return (
     <div>
       <Navigation />
+      <HeroSection />
     </div>
   );
 };
