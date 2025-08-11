2592ee927e8b795080acf073ef966e1cad3944f4
prabin738 <prabinsingh750@gmail.com>
2025-08-11 23:06:44 +0545

    Landing page is complete but not responsive will work on responsiveness later

--- Project-1/src/components/Navigation.jsx
+++ Project-1/src/components/Navigation.jsx
@@ -1,14 +1,26 @@
 const Navigation = () => {
   return (
-    <nav>
+    <nav className="container">
       <div className="logo">
         <img src="/images/brand_logo.png" alt="logo" />
       </div>
       <ul>
-        <li href="#">Home</li>
-        <li href="#">About</li>
-        <li href="#">Location</li>
-        <li href="#">Contact</li>
+        <li>
+          {" "}
+          <a href="https://prabinsinghthakuri.com.np">Home</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://prabinsinghthakuri.com.np">Contact</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://prabinsinghthakuri.com.np">Location</a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://prabinsinghthakuri.com.np">About</a>
+        </li>
       </ul>
       <button>Login</button>
     </nav>
