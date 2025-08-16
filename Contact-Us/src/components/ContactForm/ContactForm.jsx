de9524069c6101bc77c9643b3250e6f9890bf43b
prabin738 <prabinsingh750@gmail.com>
2025-08-16 15:16:25 +0545

    Contact form done

--- Contact-Us/src/components/ContactForm/ContactForm.jsx
+++ Contact-Us/src/components/ContactForm/ContactForm.jsx
@@ -1,17 +1,54 @@
-import React from "react";
 import styles from "./ContactForm.module.css";
 import Button from "../Button/Button";
 import { MdMessage } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
 
 const ContactForm = () => {
   return (
     <section className={styles.container}>
       <div className={styles.contact_form}>
-        <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px" />} />
-        <Button text="VIA SUPPORT CHAT" icon={<FaPhoneAlt fontSize="24px" />} />
+        <div className={styles.top_btn}>
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<FaPhoneAlt fontSize="24px" />}
+          />
+        </div>
+        <Button
+          isOutline={true}
+          text="VIA EMAIL FORM"
+          icon={<HiMail fontSize="24px" />}
+        />
+        <form>
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="text">text</label>
+            <textarea name="text" rows="8" />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+            }}
+          >
+            <Button text="SUBMIT FORM" />
+          </div>
+        </form>
       </div>
-      <div className={styles.contact_image}></div>
+      <div className={styles.contact_image}>
+        <img src="./images/contact.svg" alt="" />
+      </div>
     </section>
   );
 };
