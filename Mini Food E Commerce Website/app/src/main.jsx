de05c3fd4734d4e3e5d62d9209f5789e891053a5
prabin738 <prabinsingh750@gmail.com>
2025-08-18 11:02:43 +0545

    foodie project setup

---
+++ Mini Food E Commerce Website/app/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    background-color: #323334;
+    min-height: 100vh;
+  }
+`;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
